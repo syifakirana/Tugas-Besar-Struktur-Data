c417acb8d1faf760551f6c1f38771372b2305f31
Ryan Muhamad Saputra <ryan.saputra20806@gmail.com>
2026-01-02 18:02:36 +0700

    Add MLL structure and CRUD operations
    Ini sudah keseluruhan untuk File library.h. Sudah ada fungsi dan prosedur yang digunakan pada BST dan Juga MLL 1-m

--- code(c++)_digital_library_mll_bst/library.h
+++ code(c++)_digital_library_mll_bst/library.h
@@ -47,8 +47,73 @@ float avgTahun(Node* root);
 // ================ BAGIAN MLL (Peminjaman Buku) =====================
 // ===================================================================
 
+// -- Definisi Struktur MLL --
 
+// -- child elemen (Buku yang dipinjam)
+struct  Peminjaman {
+    string judulBuku;
+    string tglPinjam;
+};
+typedef struct ElmPinjam *adrPinjam;
+struct ElmPinjam {
+    Peminjaman info;
+    adrPinjam next;
+};
 
+// -- Parent Element (Member Perpus)
+struct Member {
+    string idMember; // key Parent
+    string nama;
+    string noTelp;
+};
+typedef struct ElmMember *adrMember;
+struct ElmMember {
+    Member info;
+    adrMember next; // pointer ke member selanjutnya
+    adrPinjam firstPinjam; // pointer ke child
+};
+
+// -- list MLL
+struct ListMLL {
+    adrMember first;
+};
+
+// --- Primitif MLL ---
+// Dasar MLL 
+void createListMLL(ListMLL &L);
+bool isEmptyMLL(ListMLL L);
+adrMember alokasiMember(Member data);
+adrPinjam alokasiPinjam(string judul, string tgl);
+void dealokasiMember(adrMember &P);
+void dealokasiPinjam(adrPinjam &P);
+
+// Operasi CRUD Parent (Member)
+// -- insert
+void insertFirstMember(ListMLL &L, adrMember P);
+void insertLastMember(ListMLL &L, adrMember P);
+void insertAfterMember(ListMLL &L, adrMember prec, adrMember P);
+// -- delete
+void deleteFirstMember(ListMLL &L, adrMember &P);
+void deleteLastMember(ListMLL &L, adrMember &P);
+void deleteMemberByID(ListMLL &L, string idMember);
+// -- search
+adrMember searchMember(ListMLL L, string idMember);
+
+// Operasi Crud Child (Peminjaman)
+void insertPinjaman(ListMLL &L, string idMember, adrPinjam C);
+void deletePinjaman(ListMLL &L, string idMember, string judulBuku);
+
+// CHALLENGE
+// -- apakah buku sedang dipinjam oleh member tertentu
+bool isBookBorrowed(adrMember P, string judulBuku);
+// -- tampilkan seluruh data (parent beserta child)
+void showAllData(ListMLL L);
+// -- tampilkan member yang paling aktif dan sedikit aktif
+void showMostActiveMember(ListMLL L);
+void showLeastActiveMember(ListMLL L);
+// -- filter jumlah (menampilkan member dengan jumlah pinjaman tertentu)
+void showMembersWithXLoans(ListMLL L, int x);
+// -- mencari pinjaman berdasarkan judul buku
+Node* searchJudul(Node* root, string judul);
 
 #endif
-
